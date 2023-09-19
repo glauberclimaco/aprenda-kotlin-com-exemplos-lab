2656e61011a71a3628981748974d5de539bcc62b
Glauber Climaco <51976374+glauberclimaco@users.noreply.github.com>
2023-09-19 13:16:28 -0300

    Primeiro desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val userName: String, val userNumber: Int) {
 
-class Usuario
+    init {
+        require(userNumber > 0) { "O número do usuário deve ser positivo" }
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    init {
+        require(duracao > 0) { "A duração do conteúdo deve ser positiva" }
+    }
+}
+
+class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(usuario: Usuario): Boolean {
+        if (inscritos.contains(usuario)) {
+            return false
+        }
+
+        inscritos.add(usuario)
+        return true
+    }
+
+    fun listarInscritos(): List<Usuario> {
+        return inscritos.toList()
+    }
+
+    fun listarConteudos(): List<ConteudoEducacional> {
+        return conteudos.toList()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val student01 = Usuario("Glauber", 1)
+    val student02 = Usuario("Maria", 2)
+
+    val cEducacional01 = ConteudoEducacional("kotlin", 60)
+    val cEducacional02 = ConteudoEducacional("Android Studio", 60)
+    val conteudos = mutableListOf(cEducacional01, cEducacional02)
+    val formacao = Formacao("Mobile com kotlin", conteudos)
+
+    formacao.matricular(student01)
+    formacao.matricular(student02)
+
+    println(formacao)
+    println("Inscritos:")
+    formacao.listarInscritos().forEach { println(it) }
+    println("Conteúdos:")
+    formacao.listarConteudos().forEach { println(it) }
 }
